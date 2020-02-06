855916709328510b574134922da49f9f04e9f0b6
carolin.labinski <58142571+carolinlabinski@users.noreply.github.com>
2020-02-06 22:34:53 +0100

    Add files via upload

---
+++ db/seeds.rb
@@ -0,0 +1,52 @@
+
+
+require 'faker'
+Doctor.destroy_all
+Patient.destroy_all
+Appointment.destroy_all
+City.destroy_all
+JoinTableDoctorSpecialty.destroy_all
+Specialty.destroy_all
+
+20.times do
+City.create(city: Faker::Address.city)
+end
+
+10.times do
+Specialty.create(specialty: Faker::Job.field)
+end
+
+20.times do
+Doctor.create(
+  first_name: Faker::Name.first_name, 
+  last_name: Faker::Name.last_name, 
+  zip_code: Faker::Address.zip_code, 
+  city_id: City.find(rand(City.first.id..City.last.id)).id
+)
+end
+
+40.times do
+JoinTableDoctorSpecialty.create(
+  doctor_id: Doctor.find(rand(Doctor.first.id..Doctor.last.id)).id, 
+  specialty_id: Specialty.find(rand(Specialty.first.id..Specialty.last.id)).id
+)
+end
+
+80.times do
+Patient.create(
+  first_name: Faker::Name.first_name, 
+  last_name: Faker::Name.last_name, 
+  city_id: City.find(rand(City.first.id..City.last.id)).id
+)
+end
+
+150.times do
+Appointment.create(
+  date: Faker::Time.between(from: DateTime.now, to: DateTime.now + 50), 
+  doctor_id: Doctor.find(rand(Doctor.first.id..Doctor.last.id)).id, 
+  patient_id: Patient.find(rand(Patient.first.id..Patient.last.id)).id,
+  city_id: City.find(rand(City.first.id..City.last.id)).id
+)
+end
+
+
